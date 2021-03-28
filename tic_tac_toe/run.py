b3874958ce25e2c2f29fac040c68e8dce5d9fb22
github-suraj <surajkumarjaiswal053@gmail.com>
2021-03-28 17:05:33 +0530

    Option to play with computer

--- tic_tac_toe/run.py
+++ tic_tac_toe/run.py
@@ -1,12 +1,16 @@
 import os
-from random import randint
+from time import sleep
+from random import randint, choice
 from operator import itemgetter
 from termcolor import colored
 from tabulate import tabulate
 
-global players; players = list()
 global board
+global players; players = list()
+global computer; computer = False
+global level; level = 1
 symbols = ('X', 'O')
+combinations = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
 
 os.system('color')
 _color = {'X': 'green', 'O': 'magenta'}
@@ -25,12 +29,41 @@ def final_score():
         print("\nHurrah! Its a Tie.")
 
 def check_winning(win=False):
-    combinations = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
     for comb in combinations:
         comb_set = set(itemgetter(*comb)(board))
         if len(comb_set) == 1:
             win = True
     return win
+
+def level_based_position(sym_p):
+    available_nums = [n for n in board if isinstance(n, int)]
+    if level == 1: # Easy
+        return choice(available_nums) ## random.choice
+    prefer_position = (5, 1, 3, 7, 9, 2, 4, 6, 8)
+    if level == 3: # Hard
+        if (board[0] == board[8] == sym_p) or (board[2] == board[6] == sym_p):
+             prefer_position = (5, 2, 4, 6, 8, 1, 3, 7, 9)
+    for pos in prefer_position:
+        if pos in available_nums:
+            return pos
+
+def computer_input(p):
+    print("\nComputer's turn..."); sleep(level)
+    sym_c = p['symbol']
+    sym_p = [sym for sym in symbols if sym != sym_c][-1]
+    ## If Computer Can Win
+    for comb_set in combinations:
+        comb = itemgetter(*comb_set)(board)
+        s = len(set(symbols).intersection(set(comb)))
+        if s == 1 and comb.count(sym_c) == 2:
+            return list(set(comb).difference(set(symbols)))[-1]
+    ## If Player Can Win
+    for comb_set in combinations:
+        comb = itemgetter(*comb_set)(board)
+        s = len(set(symbols).intersection(set(comb)))
+        if s == 1 and comb.count(sym_p) == 2:
+            return list(set(comb).difference(set(symbols)))[-1]
+    return level_based_position(sym_p)
 
 def user_input(p):
     available_nums = [n for n in board if isinstance(n, int)]
@@ -55,8 +88,14 @@ def show_board():
 
 def play_game(p1, p2):
     player_turns = [p1 if i%2 == 0 else p2 for i in range(len(board))]
-    for p in player_turns:
-        user_input(p)
+    for idx, p in enumerate(player_turns):
+        if idx % 2 == 0:
+            user_input(p)
+        elif computer:
+            n = computer_input(p)
+            board[n-1] = p['symbol']
+        else:
+            user_input(p)
         show_board()
         win = check_winning()
         if win:
@@ -70,24 +109,43 @@ def play_game(p1, p2):
             player['draw'] += 1
         _print(' Match DRAW ')
 
-def start_game():
+def start_game(rpi=0): # rpi=0, Only player will start game
     global board
     board = list(range(1, 10))
     _players = players.copy()
-    rpi = randint(0, 1)
+    if not computer:
+        rpi = randint(0, 1) # Random player will start game
     player1 = _players.pop(rpi)
     player2 = _players.pop()
     print(f"\n{player1['name']} will start the game!")
     show_board()
     play_game(player1, player2)
 
-def initialize_game():
-    _print(" Welcome to Tic Tac Toe Game ")
-    for i in range(2):
-        player = str(input(f"Enter Player {i+1} Name: "))
-        players.append({'name': player, 'won': 0, 'draw': 0})
+def initialize_computer_mode():
+    global level
+    print("\t1. Easy (Beginner)\n\t2. Medium (Normal)\n\t3. Hard (Impossible)\n")
+    while True:
+        try:
+            level = int(input("Choose difficulty level: "))
+            if level not in (1, 2, 3):
+                print(colored("\tPlease enter either 1, 2 or 3,", 'red'), 'Try Again!')
+                continue
+            break
+        except:
+            print(colored("\tPlease enter either 1, 2 or 3,", 'red'), 'Try Again!')
+    player = str(input("\nEnter Your Name: "))
+    players.append({'name': player, 'won': 0, 'draw': 0})
+    players.append({'name': 'Computer', 'won': 0, 'draw': 0})
 
-    rpi = randint(0, 1)
+def initialize_game(rpi=0): # rpi=0, Only player will select Symbol
+    if computer:
+        initialize_computer_mode()
+    else:
+        for i in range(2):
+            player = str(input(f"Enter Player {i+1} Name: "))
+            players.append({'name': player, 'won': 0, 'draw': 0})
+        rpi = randint(0, 1) # Random player will select Symbol
+
     common = ([0, 1], list(symbols))
     while True:
         symbol = str(input(f"\t{players[rpi]['name']}: Select your symbol of choice (X / O): "))
@@ -98,7 +156,27 @@ def initialize_game():
         else:
             print("\t\tInCorrect Input, Try again!")
 
+def choose_mode():
+    global computer
+    _print(" Welcome to Tic Tac Toe Game ")
+    print("\t1. Play with computer\n\t2. Play with friend\n")
+    while True:
+        mode = input("Choose a Mode: ")
+        try:
+            if int(mode) not in (1, 2):
+                print(colored("\tPlease enter either 1 or 2,", 'red'), 'Try Again!')
+                continue
+            if int(mode) == 1:
+                computer = True
+                _print(" You will be playing with Computer ")
+            else:
+                _print(" You will be playing with Friend ")
+            break
+        except:
+            print(colored("\tPlease enter either 1 or 2,", 'red'), 'Try Again!')
+
 def main():
+    choose_mode()
     initialize_game()
     while True:
         start_game()
